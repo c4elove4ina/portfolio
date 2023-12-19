20d11f10ab5ffdd802c9a9697f803082b7247c49
c4elove4ina <126550982+c4elove4ina@users.noreply.github.com>
2023-12-19 20:00:07 +1000

    Add files via upload
    thjsrtjhsrtjh[thj[trj[oertjhoitjotr

---
+++ assets/js/index.js
@@ -0,0 +1,32 @@
+console.log("hello world")
+
+var name ="chikibambony";
+let name_2 ="fnaf";
+const name_3 ="djegr champion";
+
+console.log(name, name_2, name_3);
+
+let a = prompt()
+if (a>18) {
+    console.log("можно");
+} else {
+    console.log("нельзя");
+
+}
+
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+let ac= 0;
+while (ac < 10) {
+    console.log(a++)
+
+}
+
+
+
+
+
+
