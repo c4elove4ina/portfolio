7a0340667ec52e9ef7c8250a01e7d7fe313d9f4c
c4elove4ina <126550982+c4elove4ina@users.noreply.github.com>
2024-01-09 19:52:39 +1000

    Add files via upload
    tr';hrtpohjho[ejh[oe5jh

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,29 @@
-console.log("hello world")
+// console.log("hello world")
 
-var name ="chikibambony";
-let name_2 ="fnaf";
-const name_3 ="djegr champion";
+// var name ="chikibambony";
+// let name_2 ="fnaf";
+// const name_3 ="djegr champion";
 
-console.log(name, name_2, name_3);
+// console.log(name, name_2, name_3);
 
-let a = prompt()
-if (a>18) {
-    console.log("можно");
-} else {
-    console.log("нельзя");
+// let a = prompt()
+// if (a>18) {
+//     console.log("можно");
+// } else {
+//     console.log("нельзя");
 
-}
+// }
 
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
     
-}
-let ac= 0;
-while (ac < 10) {
-    console.log(a++)
+// }
+// let ac= 0;
+// while (ac < 10) {
+//     console.log(a++)
 
-}
+// }
 
 
 
